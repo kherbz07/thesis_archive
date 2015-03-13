17cb4472207113bda800238fe232cce4eb71fbe0
Jerome Noveda <noveda.jerome@gmail.com>
2015-03-13 15:55:42 +0800

    database updated

--- database/db_thesis_archive.sql
+++ database/db_thesis_archive.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2015 at 08:46 AM
+-- Generation Time: Mar 13, 2015 at 08:55 AM
 -- Server version: 5.5.34
 -- PHP Version: 5.4.22
 
@@ -19,6 +19,32 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_thesis_archive`
 --
+CREATE DATABASE IF NOT EXISTS `db_thesis_archive` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `db_thesis_archive`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_category`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_category` (
+  `id` int(10) unsigned NOT NULL,
+  `category` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_course`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_course` (
+  `id` int(10) unsigned NOT NULL,
+  `course` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -43,6 +69,18 @@ CREATE TABLE IF NOT EXISTS `tbl_researchers` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_roles`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_roles` (
+  `id` int(10) unsigned NOT NULL,
+  `role` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_thesis`
 --
 
@@ -58,6 +96,49 @@ CREATE TABLE IF NOT EXISTS `tbl_thesis` (
   PRIMARY KEY (`id`),
   KEY `research_id` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_users` (
+  `id` int(10) unsigned NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  `user_info_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`,`user_info_id`),
+  KEY `user_info_id` (`user_info_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user_info`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_user_info` (
+  `id` int(10) unsigned NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `middle_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_year`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_year` (
+  `id` int(10) unsigned NOT NULL,
+  `year` varchar(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Constraints for dumped tables
@@ -76,6 +157,13 @@ ALTER TABLE `tbl_researchers`
 --
 ALTER TABLE `tbl_thesis`
   ADD CONSTRAINT `tbl_thesis_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `tbl_category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbl_users`
+--
+ALTER TABLE `tbl_users`
+  ADD CONSTRAINT `tbl_users_ibfk_2` FOREIGN KEY (`user_info_id`) REFERENCES `tbl_user_info` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `tbl_roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
